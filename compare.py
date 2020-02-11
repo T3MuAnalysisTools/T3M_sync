81682b0ca6ed293774db302f013cc15c58f0239e
Vladimir <vladimir.cherepanov@cern.ch>
2020-02-11 14:36:37 +0100

    upd latest comprsns

--- compare.py
+++ compare.py
@@ -111,7 +111,7 @@ def interSect(tree1, tree2, var='evt', common=False, save=False,  titles=[]):
     return tlist1, tlist2
 
 
-def scanForDiff(tree1, tree2, branch_names, scan_var='pt_1', index_var='evt'):
+def scanForDiff(tree1, tree2, branch_names, scan_var='Ptmu1', index_var='evt'):
     # tree1.BuildIndex(index_var)
     # index1 = tree1.GetTreeIndex()
     tree2.BuildIndex(index_var)
@@ -156,7 +156,7 @@ if __name__ == '__main__':
     parser.add_option('-c', '--no-common', dest='do_common', action='store_false', default=True, help='Do not produce plots for events in common')
     parser.add_option('-r', '--no-ratio', dest='do_ratio', action='store_false', default=True, help='Do not show ratio plots')
     parser.add_option('-d', '--diff', dest='do_diff', action='store_true', default=False, help='Print events where single variable differs')
-    parser.add_option('-v', '--var-diff', dest='var_diff', default='pt_1', help='Variable for printing single event diffs')
+    parser.add_option('-v', '--var-diff', dest='var_diff', default='Ptmu1', help='Variable for printing single event diffs')
 
     (options,args) = parser.parse_args()
 
